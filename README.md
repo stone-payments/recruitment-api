0b5fc55e6756687812584f27a855978a68b2d262
JGabrielFreitas <jgabrielafreitas@gmail.com>
2017-06-09 19:49:12 -0300

    uploading xlsx python dependence

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # recruitment-api
 stone recruitment api
+
+[xlsx with python](https://openpyxl.readthedocs.io/en/default/)
--- recruitment.py
+++ recruitment.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Victoper, só faz o GET da api loka ae, bora bora'
 
+@app.route('/candidates', methods=['GET'])
+def get_candidates():
+    return '<h1>HUE BR</h1>'
+
 
 if __name__ == '__main__':
     app.run()
