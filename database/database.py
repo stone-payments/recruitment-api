c1edf2248bada3f8176d3bdb1fe8fcbe3cf397bd
JGabrielFreitas <jgabrielafreitas@gmail.com>
2017-08-23 07:53:14 -0300

    local host database

--- database/database.py
+++ database/database.py
@@ -26,8 +26,8 @@ class DatabaseStatus(Enum):
 
 class Dao:
     def connect(self):
-        # self.client = MongoClient('localhost', 27017)
-        self.client = MongoClient(os.environ['MONGO'])
+        self.client = MongoClient('localhost', 27017)
+        # self.client = MongoClient(os.environ['MONGO'])
 
     def close_connection(self):
         self.client.close()
